88e949e718355b5d2814cf0609e092f5fb9877b1
Ricky Feng <ricky.feng@163.com>
2019-05-01 22:20:45 +0800

    商品分类和商品管理页面增加跳转到前端功能

--- admin/controller/catalog/category.php
+++ admin/controller/catalog/category.php
@@ -210,7 +210,8 @@ class ControllerCatalogCategory extends Controller {
 				'name'        => $result['name'],
 				'sort_order'  => $result['sort_order'],
 				'edit'        => $this->url->link('catalog/category/edit', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true),
-				'delete'      => $this->url->link('catalog/category/delete', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true)
+				'delete'      => $this->url->link('catalog/category/delete', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true),
+				'view'        => $this->front_url->link('product/category', 'path=' . $result['category_id'] . $url, true),
 			);
 		}
 
