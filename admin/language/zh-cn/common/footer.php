cec466699ca73bfc2fd1b24d592a54f6e0343e8d
Ricky Feng <ricky.feng@163.com>
2019-05-03 16:35:21 +0800

    还原官方域名

--- admin/language/zh-cn/common/footer.php
+++ admin/language/zh-cn/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.cn">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有';
 $_['text_version'] = '版本号 %s';
