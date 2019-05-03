f33ecc40ef4ea3b079590745410c9847edf9f1f9
Ricky Feng <ricky.feng@163.com>
2019-05-03 16:23:06 +0800

    增加城市/区县功能

---
+++ admin/language/en-gb/localisation/city.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']		= 'City';
+
+// Text
+$_['text_success']		= 'Success: City have been modified!';
+$_['text_list']         = 'City List';
+$_['text_add']          = 'Add City';
+$_['text_edit']         = 'Modify City';
+$_['text_city_area']    = 'Province';
+$_['text_city']         = 'City';
+$_['text_county']       = 'Area / County';
+
+// Column
+$_['column_name']		= 'County Name';
+$_['column_zone']		= 'Province';
+$_['column_status']		= 'Status';
+$_['column_action']		= 'Action';
+
+// Entry
+$_['entry_name']		= 'City Name：';
+$_['entry_country']		= 'Country：';
+$_['entry_zone']		= 'Province：';
+$_['entry_status']		= 'Status：';
+$_['entry_parent']		= 'Upper Level：';
+$_['text_province']		= 'Province';
+$_['text_city']			= 'City';
+$_['entry_filter_name']	= 'Search keywords';
+$_['text_search']		= 'Filter';
+$_['column_city']		= 'City';
+$_['column_type']       = 'Type';
+
+// Error
+$_['error_permission']	= 'Warning：you haven\'t permission to modify city！';
+$_['error_name']		= 'city name\'s characters must between 3 and 128 characters！';
+$_['error_zone']		= 'please select province！';
+$_['error_country']		= 'please select country！';
